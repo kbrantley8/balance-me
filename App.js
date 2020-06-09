c3c379c341751d649692b48e22d0d4b63a9e2db5
mgomez <mgomez46@gatech.edu>
2020-06-09 15:45:19 -0400

    testing

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
+import CustomButton from 'components/button.js';
 
 const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
@@ -9,9 +10,10 @@ const instructions = Platform.select({
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
+      <Text style={styles.welcome}>Welcome to React Native!!</Text>
+      <Text style={styles.instructions}>To get started, edit App.js!!</Text>
       <Text style={styles.instructions}>{instructions}</Text>
+      <CustomButton/>
     </View>
   );
 }
---
+++ components/button.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import {
+  Text
+} from 'react-native';
+
+export default class CustomButton extends Component {
+
+  render() {
+      return(
+       <Text>Hello!</Text>
+      );
+    }
+}
