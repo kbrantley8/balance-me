7c61b1ca505ca9e392634e5ea87ebd385a92df36
mgomez <mgomez46@gatech.edu>
2020-06-11 16:10:30 -0400

    button component

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
 import * as React from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
-import CustomButton from './components/button.js';
-
-const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
-});
+import PrimaryButton from './components/button.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js!!</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
-      <CustomButton/>
+      <Text style={styles.welcome}>Welcome to React Native!</Text>
+      <PrimaryButton
+        text='Its morgan' 
+        color='#FFFFFF'
+      />
     </View>
   );
 }
--- components/button.js
+++ components/button.js
@@ -1,15 +1,58 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-import {
-  Text
-} from 'react-native';
 
-class CustomButton extends Component {
+export default class PrimaryButton extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  onPress = () => {
+    this.props.onPress();
+  };
 
   render() {
-      return(
-       <Text>Hello!</Text>
-      );
-    }
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity
+          onPress={this.onPress}
+          style={styles.button}
+        >
+        <Text style={styles.text}>{this.props.text}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
-export default CustomButton;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: '#F2CD5C',
+    padding: 10,
+    borderRadius: 5,
+    width: 300,
+  },
+  text: { 
+    color: '#FFFFFF',
+    fontSize: 14
+  }
+});
+
+//puts restrictions on what type each prop can be
+PrimaryButton.propTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string,
+  onPress: PropTypes.func,
+};
+
+// what will the default be if none is specified
+PrimaryButton.defaultProps = {
+  text: 'hello',
+  color: '#FFFFFF',
+  onPress: () => {}
+}
