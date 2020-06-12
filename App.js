5d87a5f4bff263fe2e42b4f273b85f0c64fbdf49
Patel, Rahul <rpatel397@gatech.edu>
2020-06-12 16:36:16 -0400

    navigation working

--- App.js
+++ App.js
@@ -1,32 +1,43 @@
-import * as React from 'react';
-import 'react-native-gesture-handler';
-import { StyleSheet, Text, View } from 'react-native';
-import WelcomeScreen from './app/screens/WelcomeScreen'
+import * as React from "react";
+import "react-native-gesture-handler";
+import { StyleSheet, Text, View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import CreateTask from "./app/screens/CreateTask";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <WelcomeScreen name="Morgan"/>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
+        {/* {(props) => <WelcomeScreen {...props} name="Morgan" />}
+        </Stack.Screen> */}
+        <Stack.Screen name="CreateTask" component={CreateTask} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    // <View style={styles.container}>
+    //   <WelcomeScreen name="Morgan" />
+    // </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
   },
 });
 
-
-    // <NavigationContainer>
-    //     <Stack.Navigator>
-    //     <Stack.Screen
-    //       name="WelcomeScreen"
-    //       component={WelcomeScreen}
-    //     />
-    //     <Stack.Screen name="CreateTask" component={CreateTask} />
-    //   </Stack.Navigator>
-    // </NavigationContainer>
+{
+  /* <NavigationContainer>
+  <Stack.Navigator>
+    <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
+    <Stack.Screen name="CreateTask" component={CreateTask} />
+  </Stack.Navigator>
+</NavigationContainer>; */
+}
