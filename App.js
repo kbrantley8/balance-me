d24172b440570bcc16f71279ebe8c8ab616f958e
mgomez <mgomez46@gatech.edu>
2020-06-11 20:25:05 -0400

    Welcome Screen

--- App.js
+++ App.js
@@ -1,15 +1,11 @@
 import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import PrimaryButton from './components/button.js';
+import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <PrimaryButton
-        text='Its morgan' 
-        color='#FFFFFF'
-      />
+      <WelcomeScreen name="Morgan"/>
     </View>
   );
 }
@@ -20,15 +16,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
