44583da56b56f9a2c15c10625a8ac24480350cc6
Patel, Rahul <rpatel397@gatech.edu>
2020-07-10 12:42:50 -0400

    Loading icon made bigger

--- app/screens/InitialLoading.js
+++ app/screens/InitialLoading.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { userStorage } from "../backend/local_storage/userStorage";
-import { ActivityIndicator } from "react-native";
+import { ActivityIndicator, SafeAreaView, Platform } from "react-native";
 import { Context as AppContext } from "../context/appContext";
 export default class InitialLoading extends Component {
   constructor(props) {
@@ -20,7 +20,7 @@ export default class InitialLoading extends Component {
       if (user == null) {
         this.props.navigation.navigate("FirstTimeUser");
       } else {
-        await this.context.fetchData("mgomez@gmail.com");
+        await this.context.fetchData(user.email);
         this.props.navigation.navigate("WelcomeScreen");
       }
     } catch (error) {
@@ -29,7 +29,14 @@ export default class InitialLoading extends Component {
   }
 
   render() {
-    return <ActivityIndicator />;
+    return (
+      <SafeAreaView style={{ flex: 1, justifyContent: "center" }}>
+        <ActivityIndicator
+          size={Platform.OS == "ios" ? "large" : 50}
+          color="#37C1FF"
+        />
+      </SafeAreaView>
+    );
   }
 }
 
